efce96863f542385468c855ba34f64d6eb912d3e
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2023-11-07 08:14:39 -0300

    loading y error para el soap

--- src/app/soap/page.jsx
+++ src/app/soap/page.jsx
@@ -6,19 +6,24 @@ import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import SimpleSpiner from "@/components/ui/simple-spiner";
 
 export default function Page() {
     const [codigo, setCodigo] = useState('');
     const [rta, setRta] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const consulta = async()=>{
+        setLoading(true)
         await axios.post('api/consume-soap-service', {param_country:codigo})
         .then((res)=>{
             setRta(res.data);
             console.log(res.data);
+            setLoading(false);
         })
         .catch((e)=>{
             console.log(e.response.data.error);
+            setLoading(false);
         })
     }
 
@@ -30,10 +35,11 @@ export default function Page() {
                     value={codigo}
                     onChange={(e) => { setCodigo(e.target.value) }}
                     type={"text"} />
-                <Button onClick={()=>consulta()}>Buscar</Button>
-                {rta != null ?
+                <Button onClick={()=>consulta()}>Buscar {loading && <SimpleSpiner/>}</Button>
+                {rta != null && rta.FullCountryInfoResult.sName != "Country not found in the database"?
                     <Image src={rta.FullCountryInfoResult.sCountryFlag} alt="imagen" width={400} height={400}/>                    
                 : <></>}
+                {rta != null && rta.FullCountryInfoResult.sName == "Country not found in the database" && !loading? <div>No se ha encontrado información del país</div> : <></>}
             </div>
         </div>
     )
